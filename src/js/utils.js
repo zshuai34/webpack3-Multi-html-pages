3dad4b856e3e0e61e43c0f298ed6dc56aabe8c9e
zhaoshuai <zhaoshuai@ysjhacc.com>
2020-12-03 17:51:13 +0800

    添加全局环境变量

--- src/js/utils.js
+++ src/js/utils.js
@@ -1,4 +1,8 @@
-export const AJAX_URL = 'http://aixiaodou.cn'
+const AJAX_URL_DEV = 'http://dev.aixiaodou.cn'
+const AJAX_URL_TEST = 'http://test.aixiaodou.cn'
+const AJAX_URL_PROD = 'http://www.aixiaodou.cn'
 
 // webpack中定义的全局变量 当前的运行环境【生产/开发/测试】
 console.log(`当前运行环境：${NODE_ENV_PUBLIC}`)
+// 根据不同的运行环境配置不同的ajax请求地址
+export const AJAX_URL = NODE_ENV_PUBLIC === 'production' ? AJAX_URL_PROD : (NODE_ENV_PUBLIC === 'test' ? AJAX_URL_TEST : AJAX_URL_DEV)
